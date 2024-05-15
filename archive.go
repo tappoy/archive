382d09dfeeeac85d15c2d54e2af55d0b195384f5
tappoy <tappoy1234@gmail.com>
2024-05-16 06:14:02 +0900

    add NewClientFrom{Config,String}

--- archive.go
+++ archive.go
@@ -16,7 +16,12 @@
 package archive
 
 import (
+	"bufio"
 	"errors"
+	"fmt"
+	"strings"
+
+	"github.com/tappoy/archive/aws"
 	"github.com/tappoy/archive/openstack"
 	"github.com/tappoy/archive/types"
 )
@@ -24,15 +29,92 @@ import (
 var (
 	// ErrUnsupportedProtocol is returned when the protocol is not supported.
 	ErrUnsupportedProtocol = errors.New("unsupported protocol")
+
+	// ErrMissingProtocol is returned when the protocol is missing.
+	ErrMissingProtocol = errors.New("missing protocol")
 )
 
+// NewClientFromConfig creates a new client from a configuration map.
+//
+// Errors:
+//   - ErrMissingProtocol: when the "ARCHIVE_PROTOCOL" key is missing.
+//   - ErrUnsupportedProtocol: when the protocol is not supported.
+//   - Any error returned by the client constructor.
 func NewClientFromConfig(config map[string]string) (types.Client, error) {
-	switch config["ARCHIVE_PROTOCOL"] {
+	protocol, ok := config["ARCHIVE_PROTOCOL"]
+	if !ok {
+		return nil, ErrMissingProtocol
+	}
+	switch protocol {
 	case "openstack":
 		return openstack.NewClientFromConfig(config)
-	// case "aws":
-	// 	return aws.NewClientFromConfig(config)
+	case "aws":
+		return aws.NewClientFromConfig(config)
 	default:
 		return nil, ErrUnsupportedProtocol
 	}
+}
+
+// ParseError is an error type for parsing configuration.
+type ParseError struct {
+	// LineNo is the line number where the error occurred.
+	LineNo int
+	// Line is the line where the error occurred.
+	Line string
+	// Message is the error message.
+	Message string
+}
+
+// Error returns the error message.
+func (e *ParseError) Error() string {
+	return fmt.Sprintf("line %d: %s. %s", e.LineNo, e.Message, e.Line)
+}
+
+// NewClientFromString creates a new client from a configuration string.
+//
+// Errors:
+//   - ParseError: when the configuration string is invalid.
+//   - ErrUnsupportedProtocol: when the protocol is not supported.
+//   - ErrMissingProtocol: when the protocol is missing.
+//   - Any error returned by the client constructor.
+func NewClientFromString(src string) (types.Client, error) {
+	reader := bufio.NewReaderSize(strings.NewReader(src), 4096)
+
+	// read line by line
+	config := make(map[string]string)
+
+	lineNo := 0
+	for {
+		lineNo++
+		bytes, _, err := reader.ReadLine()
+		// check EOF
+		if err != nil {
+			break
+		}
+		line := string(bytes)
+
+		// check empty line
+		if len(line) == 0 {
+			continue
+		}
+
+		// check comment
+		if line[0] == '#' {
+			continue
+		}
+
+		// parse line
+		fields := strings.Fields(line)
+
+		// check key-value pair
+		if len(fields) != 2 {
+			return nil, &ParseError{lineNo, line, "invalid key-value pair"}
+		}
+
+		// set key-value pair
+		config[fields[0]] = fields[1]
+	}
+
+	// create client
+	return NewClientFromConfig(config)
 }
